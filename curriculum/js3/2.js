bb2b664200c8126d8e70a860e8db6cf2a167d406
irinamityugova <info@irinamityugova.me>
2018-06-01 18:54:12 -0700

    Exercise JS3_2

--- curriculum/js3/2.js
+++ curriculum/js3/2.js
@@ -4,8 +4,20 @@
  * @return {number}
  */
 
-const solution = (arr)=>{
-  return 0;
+const solution = (arr, obj={}, i=0)=>{
+  
+  //The exit sequence finds the key in accordance to the greatest value in the counting object.
+  if(arr[i] === undefined) return Object.keys(obj).find(key => obj[key] === Math.max(...Object.values(obj)));
+  
+  //First If-Statement initiates a key.
+  if(!obj[arr[i]]) {
+    obj[arr[i]]= 0;}
+
+  //Second If-Statement counts the character at the key-index it initiated at the first If-Statement.
+  obj[arr[i]]+=1;
+  
+  //Recursive return loops through the array.
+  return solution(arr, obj, i+1)
 };
 
 module.exports = {
