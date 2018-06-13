9b88f8a281713f6fc6e15d349c44745192b678e9
irinamityugova <info@irinamityugova.me>
2018-06-12 18:30:12 -0700

    Exercise JS4_4: Only users

---
+++ curriculum/end2end/4.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+let users = "";
+
+const handle = (req, res) => {
+  fs.readdir("/home", (err, files) => {
+    files.forEach(file => {
+      users = users+"<h2>"+ file + "</h2>";
+    });
+    console.log(users);
+    res.send(`
+    ${users}
+    `);
+  if (err) throw err;
+  });
+};
+app.get('/', handle);
+app.listen(5569);
