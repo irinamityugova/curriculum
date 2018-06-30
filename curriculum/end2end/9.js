764533c5bc3d72e480592dc0d52d98799074a0ec
irinamityugova <info@irinamityugova.me>
2018-06-30 16:05:28 -0700

    Exercise JS4_9: Take a Picture

---
+++ curriculum/end2end/9.js
@@ -0,0 +1,42 @@
+/* 
+ * Create Meme Chat. Photographs a person by having a canvas that draws the picture taken from the camera display. 
+ * Use image magic library to create a meme that thakes in the input from the user.
+ * 1. Create a chat with login page, so it takess in aa name of a person.
+ * 2. Create a live video that accesses the camera.
+ * 3. Create a canvas that takes a snapshot on the "submit" click.
+ * 4. Have a database that writes objects with name, picture, and comment information to put those up when the chat loads.
+ *
+ * Take a Picture.
+ * 1. Video
+ * 2. Submit button
+ * 3. Resulting canvas.
+ */
+
+const express = require("express");
+const app = express();
+
+app.get("/takePicture", (req, res) => {
+  res.send(`
+  <h1>Take a Picture</h1>
+  <video style="height: 200px; width: 350px;" id="video" autoplay></video>
+  <input type="text" id="input" />
+  <button id="submit">Submit</button>
+  <canvas style="height: 480; width: 640;" id="myCanvas"></canvas>
+  <script>
+  const video = document.getElementById('video');
+  const canvas = document.getElementById('myCanvas');
+  const context = canvas.getContext("2d");
+
+  navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+  });
+
+  document.getElementById("submit").addEventListener("click", () => {
+  context.drawImage(video, 0, 0, 640, 480);
+  });
+  </script>
+  `)
+})
+
+app.listen(5569)
